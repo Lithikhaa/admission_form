82c4a6ba37887eaec77fa4b72c481599c7e8eeb0
Lithikhaa <94103825+Lithikhaa@users.noreply.github.com>
2024-05-20 16:51:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import Loader from "./components/Loader";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [MoreOptionToggle, setMoreOptionToggle] = useState(false);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setLoading(true);
+    setMoreOptionToggle(false);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [pathname]);
+
+  return (
+    <>
+      {loading && <Loader loading={loading} />}
+      <ScrollToTop />
+      <Navbar
+        NavLink={NavLink}
+        MoreOptionToggle={MoreOptionToggle}
+        setMoreOptionToggle={setMoreOptionToggle}
+      />
+      <Outlet className="overflow-hidden" />
+      {/* <Footer
+        NavLink={NavLink}
+        MoreOptionToggle={MoreOptionToggle}
+        setMoreOptionToggle={setMoreOptionToggle}
+      /> */}
+    </>
+  );
+}
+
+const NavLink = [
+  {
+    id: "01",
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: "02",
+    name: "Courses",
+    link: "/courses",
+  },
+  {
+    id: "03",
+    name: "About",
+    link: "/about",
+  },
+  {
+    id: "04",
+    name: "Contact",
+    link: "/contact",
+  },
+  {
+    id: "05",
+    name: "More",
+    link: [
+      { Step: "/step" },
+      { "Cell/Club": "/club" },
+      { "Student Corner": "/student-corner" },
+      // { News: "/news" },
+      // { Events: "/event" },
+      { Gallery: "/gallery" },
+      // { Saptadhara: "/saptadhara" },
+    ],
+  },
+];
+
+export default App;
---
+++ index.js
@@ -0,0 +1,230 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+
+// redux store
+import { Provider } from "react-redux";
+import store from "./app/store";
+
+// pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Forgot from "./pages/Forgot";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Courses from "./pages/Courses";
+import Register from "./pages/Register";
+import SignUpMessage from "./pages/test/SignUpMessage";
+
+//more pages
+import Club from "./pages/more/Club";
+import Event from "./pages/more/Event";
+import Gallery from "./pages/more/Gallery";
+import News from "./pages/more/News";
+import Saptadhara from "./pages/more/Saptadhara";
+import Step from "./pages/more/Step";
+import StudentCorner from "./pages/more/StudentCorner";
+
+// student
+import StudentDashboard from "./modules/student/pages/Dashboard";
+import StudentHome from "./modules/student/pages/Home";
+import Document from "./modules/student/pages/Document";
+import Exam from "./modules/student/pages/Exam";
+import Payment from "./modules/student/pages/Payment";
+import Report from "./modules/student/pages/Report";
+
+// admin
+import AdminDashboard from "./modules/admin/pages/Dashboard";
+import Main from "./modules/admin/pages/Main";
+import Student from "./modules/admin/pages/Student";
+import Faculty from "./modules/admin/pages/Faculty";
+import Notice from "./modules/admin/pages/Notice";
+import Feedback from "./modules/admin/pages/Feedback";
+import Department from "./modules/admin/pages/Department";
+import Error from "./modules/admin/pages/Error";
+// import Gallery from "./modules/admin/pages/Gallery";
+
+// errors
+import NotFound from "./errors/Error_404";
+import NetworkIssue from "./errors/NetworkIssue";
+
+//  Courses
+import BE from "./pages/BE";
+import Business from "./pages/Business";
+import Btech from "./pages/Btech";
+import Verification from "./modules/admin/components/Verification";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        // loader: loader,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUs />,
+      },
+      {
+        path: "/courses",
+        element: <Courses />,
+      },
+      {
+        path: "/courses/BE",
+        element: <BE />,
+      },
+      {
+        path: "/courses/bba",
+        element: <Business />,
+      },
+      {
+        path: "/courses/Btech",
+        element: <Btech />,
+      },
+      {
+        path: "/signup",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/forgot",
+        element: <Forgot />,
+      },
+
+      // more pages
+      {
+        path: "/club",
+        element: <Club />,
+      },
+      {
+        path: "/event",
+        element: <Event />,
+      },
+      {
+        path: "/gallery",
+        element: <Gallery />,
+      },
+      {
+        path: "/news",
+        element: <News />,
+      },
+      {
+        path: "/saptadhara",
+        element: <Saptadhara />,
+      },
+      {
+        path: "/step",
+        element: <Step />,
+      },
+      {
+        path: "/student-corner",
+        element: <StudentCorner />,
+      },
+    ],
+  },
+  {
+    path: "/signupmessage",
+    element: <SignUpMessage />,
+  },
+  {
+    path: "/student",
+    element: <StudentDashboard />,
+    children: [
+      {
+        path: "",
+        element: <StudentHome />,
+      },
+      {
+        path: "documents",
+        element: <Document />,
+      },
+      {
+        path: "reports",
+        element: <Report />,
+      },
+      {
+        path: "exams",
+        element: <Exam />,
+      },
+      {
+        path: "payment",
+        element: <Payment />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminDashboard />,
+    children: [
+      {
+        index: "admin",
+        element: <Main />,
+      },
+      {
+        path: "student",
+        element: <Student />,
+      },
+      {
+        path: "student/verify",
+        element: <Verification />,
+      },
+      {
+        path: "faculty",
+        element: <Faculty />,
+        // loader: facultyLoader,
+      },
+      {
+        path: "notice",
+        element: <Notice />,
+      },
+      {
+        path: "feedback",
+        element: <Feedback />,
+        // loader: loadData,
+      },
+      {
+        path: "department",
+        element: <Department />,
+      },
+      {
+        path: "error",
+        element: <Error />,
+      },
+    ],
+  },
+  {
+    path: "/network",
+    element: <NetworkIssue />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
